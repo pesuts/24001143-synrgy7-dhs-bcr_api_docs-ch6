39b32addaaef6bea3f3d53f3c5a99f14fa574df9
danielsinaga <daniel.sinaga.ds@gmail.com>
2024-07-07 23:33:30 +0700

    chore: edit knexconfig

--- src/config/index.ts
+++ src/config/index.ts
@@ -18,15 +18,4 @@ const knexConfig = {
   },
 };
 
-// const knexConfig = {
-//   client: "pg",
-//   connection: {
-//     user: "postgres",
-//     password: "postgres",
-//     port: 5432,
-//     host: "127.0.0.1",
-//     database: "challenge6",
-//   },
-// };
-
 export default knexConfig;
