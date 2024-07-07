835e101b06b4d740f6e496c00714a57f8d62b6a2
danielsinaga <daniel.sinaga.ds@gmail.com>
2024-07-08 00:05:52 +0700

    debug

--- src/config/index.ts
+++ src/config/index.ts
@@ -10,7 +10,6 @@ const knexConfig = {
   connection: {
     user: process.env.DB_USER!,
     password: process.env.DB_PASSWORD!,
-    // port: process.env.DB_PORT || 5432,
     port: parseInt(process.env.DB_PORT!, 10)!,
     host: process.env.DB_HOST!,
     database: process.env.DB_NAME!,
